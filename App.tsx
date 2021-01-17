f1c776035ff7d832d80fc6f530dcd99caedfc180
Fábio Alvarenga <fabio99alvarenga@gmail.com>
2021-01-17 11:49:25 -0300

    feat: add react-navigation

--- App.tsx
+++ App.tsx
@@ -1,6 +1,22 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import Routes from './src/routes';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
 export default function App() {
-	return <Routes />;
+	const Stack = createStackNavigator();
+
+	return (
+		<NavigationContainer>
+			<Stack.Navigator>
+				<Stack.Screen
+					name='Routes'
+					component={Routes}
+					options={{ headerShown: false }}
+				/>
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
 }
