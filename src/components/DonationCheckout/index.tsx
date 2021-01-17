001aa7f7cd526ef60b2cd9810f46174da3266ebd
Fábio Alvarenga <fabio99alvarenga@gmail.com>
2021-01-17 15:28:09 -0300

    build: add lottie-react-native

--- src/components/DonationCheckout/index.tsx
+++ src/components/DonationCheckout/index.tsx
@@ -1,10 +1,40 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import {
+	Container,
+	Title,
+	Subtitle,
+	Input,
+	InputDesc,
+	ThiccInput,
+	CameraBtn,
+	CameraBtnText,
+} from './styles';
+import { Picker } from '@react-native-picker/picker';
+import { Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
 
 export default function index() {
+	const navigation = useNavigation();
+	const navigateToCamera = () => {
+		navigation.navigate('DonationCamera');
+	};
+
 	return (
-		<View>
-			<Text>donation checkout</Text>
-		</View>
+		<Container>
+			<Title>Obrigado por registrar seu item ;)</Title>
+			<Subtitle>Agora põe tudo na caixa que vamos fazer a coleta</Subtitle>
+			<InputDesc>Nome</InputDesc>
+
+			<CameraBtn onPress={navigateToCamera}>
+				<Feather
+					name='camera'
+					size={50}
+					color='white'
+					style={{ alignSelf: 'center', marginTop: 30 }}
+				/>
+
+				<CameraBtnText>Agora tira uma foto bem legal pra gente!</CameraBtnText>
+			</CameraBtn>
+		</Container>
 	);
 }
---
+++ src/components/DonationCheckout/styles.tsx
@@ -0,0 +1,67 @@
+import styled from 'styled-components/native';
+import { StatusBar } from 'react-native';
+
+export const Container = styled.View`
+	display: flex;
+	flex-direction: column;
+	flex: 1;
+	background-color: #275e4d;
+	padding-left: 40px;
+	padding-right: 40px;
+`;
+
+export const Title = styled.Text`
+	font-size: 35px;
+	color: white;
+	margin-top: ${(StatusBar.currentHeight as number) + 40}px;
+	font-weight: bold;
+	width: 250px;
+`;
+
+export const Subtitle = styled.Text`
+	font-size: 25px;
+	color: white;
+	margin-top: 20px;
+	width: 100%;
+`;
+
+export const InputDesc = styled.Text`
+	font-size: 15px;
+	color: white;
+	margin-top: 20px;
+`;
+
+export const Input = styled.TextInput`
+	font-size: 18px;
+	background-color: white;
+	margin-top: 7px;
+	width: 100%;
+	height: 43px;
+`;
+
+export const PickerTouchable = styled.TouchableOpacity`
+	font-size: 25px;
+	background-color: white;
+	margin-top: 7px;
+	width: 100%;
+	height: 43px;
+	border-radius: 12px;
+`;
+
+export const ThiccInput = styled.TextInput`
+	font-size: 18px;
+	background-color: white;
+	margin-top: 7px;
+	width: 100%;
+	height: 100px;
+`;
+
+export const CameraBtn = styled.TouchableOpacity``;
+
+export const CameraBtnText = styled.Text`
+	font-size: 15px;
+	font-weight: bold;
+	color: white;
+	text-align: center;
+	margin-top: 10px;
+`;
